81caffbe61d6b26ae2df033fec7874af0011a1e3
Samuel Biondolillo <biwinning121@gmail.com>
2017-09-26 14:45:30 -0400

    Add test class for BinaryTree

---
+++ src/classes/TestBinaryTree.java
@@ -0,0 +1,17 @@
+/*
+ * BinaryTree Class
+ * Samuel Biondolillo
+ * CIS210M:ZZ Data Structures and Elementary Algorithms
+ * Goal: To create a driver class for testing the BinaryTree class
+ * Version  0.0.1   9/26/17
+ */
+
+package classes;
+
+public class TestBinaryTree {
+
+	public static void main(String[] args) {
+
+	}
+
+}
