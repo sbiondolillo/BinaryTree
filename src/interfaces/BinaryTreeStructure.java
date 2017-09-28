e32ba2a0cb3a81cc6d1828268bfc2ede153847ed
Samuel Biondolillo <biwinning121@gmail.com>
2017-09-28 11:15:31 -0400

    update BianryTreeStructure comments

--- src/interfaces/BinaryTreeStructure.java
+++ src/interfaces/BinaryTreeStructure.java
@@ -5,6 +5,7 @@
  * Goal: To create a base interface for BinaryTree objects
  * Version  0.0.1   9/26/17
  * 			0.0.2	9/27/17		Added getter for root instance variable
+ * 			0.0.3	9/28/17		Added find() method and updated comments
  */
 
 package interfaces;
@@ -23,16 +24,20 @@ public interface BinaryTreeStructure {
 	
 	/*
 	 * Add a node to the tree
+	 * @param node - a Node object to be added
 	 */
 	void add(Node node);
 	
 	/*
 	 * Modify a node in place
+	 * @param existingNode - a Node which exists in the tree
+	 * @param newNode - a Node object to be added to the tree
 	 */
 	void change(Node existingNode, Node newNode);
 	
 	/*
 	 * Remove a node from the tree
+	 * @param node - a Node in the tree to be deleted
 	 */
 	void delete(Node node);
 	
